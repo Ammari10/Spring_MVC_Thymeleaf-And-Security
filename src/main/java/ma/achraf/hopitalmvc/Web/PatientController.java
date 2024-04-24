ad67060777215da8412ce3af27911ff2e0b27ce8
Achraf AMMARI <AMMARI@gmail.com>
2024-04-24 22:19:42 +0200

    firs commit

--- src/main/java/ma/achraf/hopitalmvc/Web/PatientController.java
+++ src/main/java/ma/achraf/hopitalmvc/Web/PatientController.java
@@ -1,15 +1,20 @@
 package ma.achraf.hopitalmvc.Web;
 
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import ma.achraf.hopitalmvc.Repository.PatientRepository;
 import ma.achraf.hopitalmvc.entities.Patient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -24,7 +29,7 @@ public class PatientController {
     public String index(Model model,
 
                         @RequestParam (name = "page",  defaultValue ="0") int p,
-                        @RequestParam ( name= "size", defaultValue = "6") int s,
+                        @RequestParam ( name= "size", defaultValue = "8") int s,
                         @RequestParam  (name = "keyword",  defaultValue ="")  String kw
 
 ){ // methode qui retourne string apres je stocke dans le model avec une type model
@@ -42,6 +47,37 @@ Page<Patient> pagePatient=patientRepository.findByNomContains(kw,PageRequest.of(
     patientRepository.deleteById(id);// supprimer et apres on va revenir pour afficher  en fait une redirection
     return "redirect:/index?page="+page+"&keyword="+keyword;
     }
+@GetMapping ("/")
+    public  String home () {
+    return  "redirect :/index";
+}
+@GetMapping ("Patient")
+@ResponseBody
+public  List<Patient> listPatients (){
+    return  patientRepository.findAll();
+    }
 
+    @GetMapping("/formPatients")
+    public String formPatient(Model model ){
+    model.addAttribute("patient",new Patient());
+         return  "formPatients";
+    }
+@PostMapping (path = "/save")
+public String save(Model model, @Valid Patient patient, BindingResult bindingResult, // pour faire la validation tu doit 3 chose a faire la premier c est les inotation de Validation ajoute LE (size emty  DECIMAL MIN... et pour la troisieme c est au niveau de controlleur il faut utilise la notation Valide et BindingResult
+                   @RequestParam(defaultValue = "0")  int page,
+                   @RequestParam(defaultValue = "") String keyword){
+    if (bindingResult.hasErrors()) return "formPatients";
+    patientRepository.save(patient);
+    return "redirect:/index?page="+page+"&keyword="+keyword;
+}
+    @GetMapping("/editPatient")
+    public String editPatient(Model model, Long id, String keyword, int page){
+        Patient patient=patientRepository.findById(id).orElse(null);
+        if (patient==null) throw new RuntimeException("patient introuvable");
+        model.addAttribute("patient",patient);
+        model.addAttribute("page",page);
+        model.addAttribute("keyword",keyword);
+        return "editPatient";
 
+    }
 }
--- src/main/java/ma/achraf/hopitalmvc/entities/Patient.java
+++ src/main/java/ma/achraf/hopitalmvc/entities/Patient.java
@@ -1,25 +1,30 @@
 package ma.achraf.hopitalmvc.entities;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 @Entity
 @Data @NoArgsConstructor @AllArgsConstructor @Builder
 public class Patient {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotEmpty // je na accepte pas  que l attributs nom que empty
+    @Size(min=4, max = 40)
     private String nom;
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat (pattern = "yyyy-MM-dd" )
     private Date dateNaissance ;
     private boolean malade;
+@DecimalMin("100") // n accepte pas a score inferier A 100
     private int score;
 
 }
