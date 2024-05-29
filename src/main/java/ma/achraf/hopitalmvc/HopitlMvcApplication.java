8fa2f428324d77fd7f0c3dc81e54d8f45f1075ce
Ayoub ASRAOUI <asraoui_ayoub@emsi-edu.ma>
2024-05-29 10:59:29 +0100

    ayoubcommit

--- src/main/java/ma/achraf/hopitalmvc/HopitlMvcApplication.java
+++ src/main/java/ma/achraf/hopitalmvc/HopitlMvcApplication.java
@@ -26,7 +26,7 @@ public class HopitlMvcApplication  implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-         patientRepository.save(new Patient(null,"Shryyfa",new Date(),false,23));
+       /*  patientRepository.save(new Patient(null,"Shryyfa",new Date(),false,23));
          patientRepository.save(new Patient(null,"Ayoub",new Date(),false,223));
         patientRepository.save(new Patient(null,"Mohammed",new Date(),false,233));
          patientRepository.save(new Patient(null,"Ali",new Date(),false,233));
@@ -35,23 +35,13 @@ public class HopitlMvcApplication  implements CommandLineRunner {
         patientRepository.save(new Patient(null,"Safae",new Date(),false,233));
         patientRepository.save(new Patient(null,"Oumaima",new Date(),false,233));
         cree patient premier solution sans parametre
-
+*/
         //Patient patient = new Patient();
       //  patient.setId(null);
       //  patient.setNom("Ashraf");
       //  patient.setDateNaissance(new Date());
         // patient.setScore(23);
 
-        // deuxieme solution Avec des parametre
-        //Patient patient2 = new Patient (null,"shryyfa",new Date(),false,123);
-
-        // troisieme solution avec Builder
-        //Patient patient3 =Patient.builder()
-               // .nom("mikasa")
-               // .dateNaissance(new Date())
-              //  .score( 23)
-             ////   .Malade ( true)
-            //    .build();
 
 
 
